21f7d5af5297e01d0b20b6935b6651a95e7f4717
ch25572557-png <ch25572557@gmail.com>
2026-04-17 22:38:46 +0700

    Create ticket.py

---
+++ ticket.py
@@ -0,0 +1,30 @@
+import discord
+
+class TicketSystem:
+    def __init__(self, brain):
+        self.brain = brain
+
+    async def create(self, guild, user, reason="order"):
+
+        try:
+            category = guild.get_channel(
+                int(self.brain.get("CHANNELS.TICKET_CATEGORY"))
+            )
+        except:
+            category = None
+
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(view_channel=False),
+            user: discord.PermissionOverwrite(view_channel=True),
+            guild.me: discord.PermissionOverwrite(view_channel=True)
+        }
+
+        channel = await guild.create_text_channel(
+            name=f"ticket-{user.name}",
+            category=category,
+            overwrites=overwrites
+        )
+
+        await channel.send(f"🎫 Ticket created for {user.mention}\nReason: {reason}")
+
+        return channel
