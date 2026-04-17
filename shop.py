c9cb21ae52193e86042f7662e903ab25964f033b
ch25572557-png <ch25572557@gmail.com>
2026-04-18 01:39:04 +0700

    Update shop.py

--- shop.py
+++ shop.py
@@ -7,7 +7,7 @@ class ShopView(discord.ui.View):
         self.bot = bot
 
     @discord.ui.button(label="🛒 BUY", style=discord.ButtonStyle.green)
-    async def buy(self, interaction, button):
+    async def buy(self, interaction: discord.Interaction, button: discord.ui.Button):
 
         await self.bot.order.create(
             interaction.guild,
