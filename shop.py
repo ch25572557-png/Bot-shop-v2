844767dcb2991a4736c890d65777ad7c6f10114b
ch25572557-png <ch25572557@gmail.com>
2026-04-17 22:31:10 +0700

    Update and rename config.py to shop.py

---
+++ shop.py
@@ -0,0 +1,21 @@
+import discord
+
+class ShopView(discord.ui.View):
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+
+    @discord.ui.button(label="🛒 BUY", style=discord.ButtonStyle.green)
+    async def buy(self, interaction, button):
+
+        await self.bot.order.create(
+            interaction.guild,
+            interaction.user,
+            "ITEM"
+        )
+
+        await interaction.response.send_message(
+            "🎫 Order Created + Ticket Opened",
+            ephemeral=True
+        )
