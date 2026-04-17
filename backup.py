c9aab7e8ff9403e822c1a2f0a35338d2e804bfc8
ch25572557-png <ch25572557@gmail.com>
2026-04-18 01:58:13 +0700

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,12 +1,12 @@
-class BackupSystem:
-    def __init__(self, brain, bot):
-        self.brain = brain
-        self.bot = bot
+async def log(self, message):
+    try:
+        ch_id = self.brain.get("CHANNELS.BACKUP")
+        channel = self.bot.get_channel(int(ch_id))
 
-    async def log(self, message):
-        try:
-            ch_id = self.brain.get("CHANNELS.BACKUP")
-            channel = self.bot.get_channel(int(ch_id))
-            await channel.send(f"💾 BACKUP\n{message}")
-        except:
-            pass
+        if channel is None:
+            return
+
+        await channel.send(f"💾 BACKUP\n{message}")
+
+    except Exception as e:
+        print(f"[BACKUP ERROR] {e}")
